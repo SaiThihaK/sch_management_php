91c2a420f18e24f2d8effee0bf5e2138f36ff9f3
SaiThihaK <saithihak2@gmail.com>
2023-05-12 22:14:11 +0630

    tabs

--- private/views/components/head.view.php
+++ private/views/components/head.view.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="..private/style/main.style.css">
+    <link rel="stylesheet" href="<?php ROOT ?>css/style.css">
     <title>Student Management System</title>
     <script src="https://kit.fontawesome.com/18d690af8d.js" crossorigin="anonymous"></script>
 </head>
--- private/views/page/profile.view.php
+++ private/views/page/profile.view.php
@@ -1,6 +1,7 @@
 <?php $this->view("components/head") ?>
 <?php $this->view("components/navbar") ?>
 <div class="container-fluid w-100 h-100">
+    <!-- Profile -->
     <div style="max-width:1000px;" class="p-2 shadow mx-auto mt-3 mt-md-5">
         <div class="row">
             <div class="col-12 col-md-4 ps-3">
@@ -37,4 +38,24 @@
             </div>
         </div>
     </div>
-</div>
+    <!-- Tab  -->
+    <div style="max-width:1000px" class="ps-2 mx-auto mt-5">
+        <ul class="nav nav-tabs mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="tab" role="button" aria-controls="pills-home" aria-selected="true"><i class="fas fa-file-lines fa-fw me-2"></i>Basic Info</a>
+                <!-- <button class="nav-link active" >Home</button> -->
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false"><i class="fas fa-chalkboard-user fa-fw me-2"></i>Classes</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false"><i class="fas fa-list-check fa-fw me-2"></i>Tests</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">Basic Info</div>
+            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">Classes</div>
+            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">Tests</div>
+        </div>
+
+    </div>
